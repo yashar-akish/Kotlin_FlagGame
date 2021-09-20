b0fccdc9f46a268ba77badbc4bcedd1cd9d4e8dc
yashar-akish <yashar_akish@yahoo.com>
2021-09-20 16:59:38 -0400

    removing statusBar from screen

--- app/src/main/java/com/company/kotlin_flaggame/MainActivity.kt
+++ app/src/main/java/com/company/kotlin_flaggame/MainActivity.kt
@@ -2,10 +2,16 @@ package com.company.kotlin_flaggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * removing statusBar from screen
+         */
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
